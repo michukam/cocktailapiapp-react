6022b9192fb5c2ab4958a94616439b23b756d35a
michukam <134005791+michukam@users.noreply.github.com>
2023-05-18 20:55:57 +0200

    Add files via upload

---
+++ src/pages/About.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">🥂 O nas 🥂</h1>
+      <p>
+        Przepisy na drinki z baru "KamKon".
+        <p>
+      
+        </p>
+        <p>
+        W naszej ofercie znajduje się ponad 40 napojów alkoholowych oraz klasyków tj:
+        </p>
+      </p>
+      <p>
+        🍹 Wódka
+      </p>
+      <p>
+        🍺 Piwo
+        </p>
+        <p>
+        🍷 Wino
+        </p>
+        <p>
+        Przepisy na nasze hitowe drinki udostępniamy w internecie za darmo!
+        <p></p>Smacznego korzystania! Efekty możesz udostępnić na naszych socialmediach
+      </p>
+      
+      
+    </section>
+  );
+}
